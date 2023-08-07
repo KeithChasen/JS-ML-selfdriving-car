5ae48c66f31640888bf6718f05e050d9630e7c70
keithchasen <keithchasen89@gmail.com>
2023-08-07 15:00:29 +0200

    drawing borders of the road

--- road.js
+++ road.js
@@ -10,6 +10,17 @@ class Road {
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+
+        const topLeft =     {x: this.left, y: this.top};
+        const bottomLeft =  {x: this.left, y: this.bottom};
+        const topRight =    {x: this.right, y: this.top};
+        const bottomRight = {x: this.right, y: this.bottom};
+
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight],
+        ];
     }
 
     getLaneCenter(laneIndex) {
@@ -26,7 +37,7 @@ class Road {
         ctx.strokeStyle = 'white';
 
 
-        for (let i = 0; i <= this.laneCount; i++) {
+        for (let i = 1; i <= this.laneCount-1; i++) {
         //     // linear interpolation
         //     // interpolate from left - to - right
         //     //
@@ -37,19 +48,20 @@ class Road {
                 this.right,
                 i/this.laneCount
             );
-
-            // adding dashes
-            // if line is not left or right
-            if (i > 0 && i < this.laneCount) {
-                ctx.setLineDash([20, 20]);
-            } else {
-                ctx.setLineDash([]);
-            }
-
+        
+            ctx.setLineDash([20, 20]);
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+
+        ctx.setLineDash([]);
+        this.borders.forEach(border => {
+            tx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        })
     }
 }
