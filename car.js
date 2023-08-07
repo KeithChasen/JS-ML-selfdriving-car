71a099e2a4cda9b6f073ddb6d12eee25cb41e95f
keithchasen <keithchasen89@gmail.com>
2023-08-07 13:18:41 +0200

    flip angle for reverse movement

--- car.js
+++ car.js
@@ -47,13 +47,16 @@ class Car {
 
 
         if (this.speed != 0) {
+
+            const flip = this.speed > 0 ? 1 : -1;
+
             // user presses the X controls
             if (this.controls.left) {
-                this.angle += 0.03;
+                this.angle += 0.03 * flip;
             }
 
             if (this.controls.right) {
-                this.angle -= 0.03;
+                this.angle -= 0.03 * flip;
             }
         }
         
