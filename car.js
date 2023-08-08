1d62f4d4379457a69130f48fb760d05d8823280f
keithchasen <keithchasen89@gmail.com>
2023-08-08 23:32:00 +0200

    drawing car as set of points(polygon)

--- car.js
+++ car.js
@@ -69,30 +69,55 @@ class Car {
 
     update(roadBorders) {
         this.#move();
+        this.polygon = this.#createPoligon();
         this.sensor.update(roadBorders);
     }
 
+    #createPoligon() {
+        const points=[];
+        // square root of w and h (hypotenuse) c2 = w2 + h2
+        const rad = Math.hypot(this.width, this.height) / 2;
+        // angle by w and h - arc tangent of w and h
+        const alpha = Math.atan2(this.width, this.height);
+
+        // TOP RIGHT
+        points.push({
+            // consider (angle) of the car and alpha angle of the vertex
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad,
+        });
+
+        // TOP LEFT
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad,
+        });
+
+        // BOTTOM RIGHT
+        points.push({
+            // consider (angle) of the car and alpha angle of the vertex
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+        });
+
+        // BOTTOM LEFT
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+        });
+
+        return points;
+    }
+
+
     draw(ctx) {
-        // rotation
-        ctx.save();
-        //translate to the point where rotation should be centered at
-        ctx.translate(this.x, this.y);
-        //rotate context by minus angle
-        ctx.rotate(-this.angle);
-
-
         ctx.beginPath();
-
-        //removed x and y because we're translating to that point
-        ctx.rect(
-            - this.width / 2,
-            - this.height / 2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i=1; i<this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
 
-        ctx.restore();
 
         this.sensor.draw(ctx);
     }
