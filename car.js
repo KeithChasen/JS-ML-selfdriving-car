3fc42f43b58a419327fd12dbc29221b63e1c8862
keithchasen <keithchasen89@gmail.com>
2023-08-09 14:25:44 +0200

    car gets damage from traffic car

--- car.js
+++ car.js
@@ -71,21 +71,26 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         if (!this.damaged) {
             this.#move();
             this.polygon = this.#createPoligon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
         
         if(this.sensor) {
-            this.sensor.update(roadBorders);
+            this.sensor.update(roadBorders, traffic);
         }
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders, traffic) {
         for (let i=0; i<roadBorders.length; i++) {
             if (polysIntersect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        }
+        for (let i=0; i<traffic.length; i++) {
+            if (polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
             }
         }
